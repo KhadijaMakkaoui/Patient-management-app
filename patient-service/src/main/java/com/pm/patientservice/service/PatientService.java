b3bd556bc73bde1f16e69b36a4b7d8fee03a265a
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2025-10-21 15:26:03 +0100

    create rest api to get all patientsÃ

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,4 +1,11 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface PatientService {
+    List<PatientResponseDTO> getPatients();
 }
--- patient-service/src/main/java/com/pm/patientservice/service/imp/PatientServiceImp.java
+++ patient-service/src/main/java/com/pm/patientservice/service/imp/PatientServiceImp.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+
 public class PatientServiceImp {
     private PatientRepository patientRepository;
 
