95bde14db399ef1291143459abc5e55f77713653
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2025-10-21 15:22:12 +0100

    create patient controller

---
+++ patient-service/src/main/java/com/pm/patientservice/controller/PatientController.java
@@ -0,0 +1,12 @@
+package com.pm.patientservice.controller;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+}
