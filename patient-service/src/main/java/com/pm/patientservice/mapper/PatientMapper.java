5cf4ad35a28d96a4b9e5982b7c188d4d3afa25c0
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2025-10-22 10:51:09 +0100

    create method to convert dto to model

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -1,7 +1,10 @@
 package com.pm.patientservice.mapper;
 
+import com.pm.patientservice.dto.PatientRequestsDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
+
+import java.time.LocalDate;
 
 public class PatientMapper {
     public static PatientResponseDTO toDTO(Patient patient) {
@@ -14,4 +17,15 @@ public class PatientMapper {
 
         return patientDTO;
     }
+
+    public static Patient toModel(PatientRequestsDTO patientRequestsDTO) {
+        Patient patient = new Patient();
+        patient.setName(patientRequestsDTO.getName());
+        patient.setAdress(patientRequestsDTO.getAdress());
+        patient.setEmail(patientRequestsDTO.getEmail());
+        patient.setDateOfBirth(LocalDate.parse(patientRequestsDTO.getDateOfBirth()));
+        patient.setRegisterDate(LocalDate.parse(patientRequestsDTO.getRegistredDate()));
+        return patient;
+    }
+
 }
