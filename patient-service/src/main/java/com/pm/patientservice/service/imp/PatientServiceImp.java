6403a0de5c36db785a30835fe7cbdb1febace911
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2025-10-22 10:31:40 +0100

    fix error of DI

--- patient-service/src/main/java/com/pm/patientservice/service/imp/PatientServiceImp.java
+++ patient-service/src/main/java/com/pm/patientservice/service/imp/PatientServiceImp.java
@@ -4,13 +4,14 @@ import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
+import com.pm.patientservice.service.PatientService;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class PatientServiceImp {
+@Service
+public class PatientServiceImp implements PatientService {
     private PatientRepository patientRepository;
 
     public PatientServiceImp(PatientRepository patientRepository) {
