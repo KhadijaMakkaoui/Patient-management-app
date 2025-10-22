cb02db7dacf0adc976d588d12c6a5ab2acfcd08f
KhadijaMakkaoui <makkaoui.khadija44@gmail.com>
2025-10-22 12:49:15 +0100

    create insert method

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,6 +1,8 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientRequestsDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,4 +10,5 @@ import java.util.List;
 
 public interface PatientService {
     List<PatientResponseDTO> getPatients();
+    PatientResponseDTO createPatient(PatientRequestsDTO patientRequestsDTO);
 }
